09c5b58fec37a09e961e4db3698b92d85396a103
Damian Malysza <damian.malysza@gmail.com>
2020-11-03 19:54:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,9 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && @status = "pending"
+    if self.valid? && @status = "pending" && sender.balance >= amount
+
+    end
 
   end
 
